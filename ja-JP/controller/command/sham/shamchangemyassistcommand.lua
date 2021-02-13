2caf803e3e533dbc59f45d3a96606bf27e536e75
achu111 <78003468+achu111@users.noreply.github.com>
2021-02-12 23:07:39 -0800

    Delete ja-JP directory

--- ja-JP/controller/command/sham/shamchangemyassistcommand.lua
+++
@@ -1,6 +0,0 @@
-slot0 = class("ShamChangeMyAssistCommand", pm.SimpleCommand)
-
-function slot0.execute(slot0, slot1)
-end
-
-return slot0
--- ja-JP/view/equipment/equipmentitemtransformver.lua
+++
@@ -1,9 +0,0 @@
-slot0 = class("EquipmentItemTransformVer", import(".EquipmentItem"))
-
-function slot0.updateSelected(slot0, slot1)
-	slot0.selected = slot1
-
-	setActive(slot0.selectedMask, slot1)
-end
-
-return slot0
